9c435fe08243cdbfbac6ff67fe170a86760bf511
Jun Dolor <jun.dolor@gmail.com>
2020-03-15 23:57:07 +0800

    Added Heroku button link

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include "header.php" ?>
-    <h1>Hello World</h1>
+    <h1>Hello World!</h1>
     <p><a href="https://heroku.com"class="btn btn-success" target="_blank">Heroku.com</a></p>
 </body>
 </html>
